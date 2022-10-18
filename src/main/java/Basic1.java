a0416e5ddd54021de73cd19867360766e9dce7c2
Данислав Тодоров <DATodorov19@codingburgas.bg>
2022-10-18 09:06:17 +0300

    Initial commit

---
+++ src/main/java/Basic1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Basic1 {
+    public static void main(String[] args){
+        Human human = new Human();
+        human.setAge(12);
+        human.setName("DAD");
+        System.out.println(human.getName());
+        System.out.println(human);
+        Human human1 = new Human();
+        human1.setAge(54);
+        human1.setName("FFDSAADA");
+        System.out.println(human1.getName());
+        System.out.println(human1);
+    }
+
+}
